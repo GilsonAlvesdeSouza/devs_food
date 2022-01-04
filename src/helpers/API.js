af97bd8571786de83141664608628a78f1c3b6a6
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-01-03 23:41:28 -0300

    Estruturação da API

---
+++ src/helpers/API.js
@@ -0,0 +1,33 @@
+import Cookies from "js-cookie";
+import qs from "qs";
+
+const BASE_URL = "https://api.b7web.com.br/devsfood/api";
+
+const apiFetchGet = async (endpoint, body = []) => {
+  if (!body.token) {
+    let token = Cookies.get("token");
+    if (token) {
+      body.token = token;
+    }
+  }
+
+  const res = await fetch(`${BASE_URL + endpoint}?${qs.stringify(body)}`);
+  const json = await res.json();
+
+  if (json.notallowed) {
+    window.location.href = "/signin";
+    return;
+  }
+  return json;
+};
+
+const API = () => {
+  return {
+    getCategories: async () => {
+        const json = await apiFetchGet("/categories");
+        return json;
+    },
+  };
+};
+
+export default API;
--- src/pages/HomeScreen/index.jsx
+++ src/pages/HomeScreen/index.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { Header } from "../../components";
-import { Container, Titulo } from "./styled";
+import API from "../../helpers/API";
+import { Container } from "./styled";
 
 function HomeScreen() {
   const history = useHistory();
   const [headerSearch, setHeaderSearch] = useState("");
 
+  useEffect(() => {
+    let api = API();
+    const getCategories = async () => {
+      const json = await api.getCategories();
+      console.log(json);
+    };
+    getCategories();
+  }, []);
+  
   return (
     <Container>
       <Header search={headerSearch} onSearch={setHeaderSearch} />
