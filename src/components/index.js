50c74aefbc26bf106288acd998ef8f9fa8a62d1c
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-01-04 10:49:40 -0300

    Exibir Categorias (parte 1)

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import MenuItem from "./MenuItem";
 import PrivateRouter from "./PrivateRouter";
 import Cart from "./Cart";
 import Header from "./Header";
+import CategoryItem from "./CategoryItem";
 
-export { MenuItem, PrivateRouter, Cart, Header };
+export { MenuItem, PrivateRouter, Cart, Header, CategoryItem };
--- src/pages/HomeScreen/index.jsx
+++ src/pages/HomeScreen/index.jsx
@@ -1,25 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import { Header } from "../../components";
+import { Header, CategoryItem } from "../../components";
 import API from "../../helpers/API";
-import { Container } from "./styled";
+import { Container, CategoryArea, CategoryList } from "./styled";
 
 function HomeScreen() {
   const history = useHistory();
   const [headerSearch, setHeaderSearch] = useState("");
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     let api = API();
     const getCategories = async () => {
       const json = await api.getCategories();
-      console.log(json);
+      if (json.error === "") {
+        setCategories(json.result);
+      }
     };
     getCategories();
   }, []);
-  
+
   return (
     <Container>
       <Header search={headerSearch} onSearch={setHeaderSearch} />
+      {categories.length > 0 && (
+        <CategoryArea>
+          Selecione uma categoria
+          <CategoryList>
+            <CategoryItem
+              title="Todas as categorias"
+              image="food-and-restaurant.png"
+            />
+          </CategoryList>
+        </CategoryArea>
+      )}
     </Container>
   );
 }
