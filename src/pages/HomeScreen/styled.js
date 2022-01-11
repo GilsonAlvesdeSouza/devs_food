317f4817e0e7f63b79b86dc02bb8503f71ec9e18
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-01-11 19:35:27 -0300

    Corrigindo Scroll Estrutural

--- src/pages/HomeScreen/styled.js
+++ src/pages/HomeScreen/styled.js
@@ -1,16 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    padding: 15px;
+  width: 100%;
+  padding: 15px;
 `;
 
 export const CategoryArea = styled.div`
-    color: #FFF;
-    margin-top: 20px;
-    `;
+  color: #fff;
+  margin-top: 20px;
+  height: 2000px;
+`;
 
 export const CategoryList = styled.div`
-    display: flex;
-    margin-top: 10px;
+  display: flex;
+  margin-top: 10px;
 `;
